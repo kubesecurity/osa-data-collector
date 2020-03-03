c1cf48e0115dd474b09886f5ecc3fa8b0b90adc1
Raj Zalavadia <rzalavad@redhat.com>
2020-03-03 08:45:01 +0530

    Added Cron job related template

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,10 +3,5 @@ set +ex
 
 ecosystem_list=("openshift" "knative" "kubevirt")
 
-ls -ltr
-
-ls -ltr app
-# Run the inference for each ecosystem in the inference list.
-# for ecosystem in ${ecosystem_list[*]};do
-python3 app/run_data_collector.py -eco-systems ${ecosystem_list[*]} -days 1;
-# done
+# Run the data collector for given eco systems and no of days to colelct data
+python3 app/run_data_collector.py -eco-systems ${ecosystem_list[*]} -days ${DAYS};
